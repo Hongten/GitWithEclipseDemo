26c2389ce7183d6df964f843f082f04492c1f8ac
Hongten <hongtenzone@foxmail.com>
2017-03-04 12:31:30 +0800

    Initial checkin of xdocreport-example

---
+++ xdocreport-example/src/main/java/com/b510/hongten/xdocreport/entity/Student.java
@@ -0,0 +1,46 @@
+package com.b510.hongten.xdocreport.entity;
+
+/**
+ * @author hongten
+ * @created Mar 4, 2017
+ */
+public class Student {
+
+	private Long id;
+	private String no;
+	private String name;
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNo() {
+		return no;
+	}
+
+	public void setNo(String no) {
+		this.no = no;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
